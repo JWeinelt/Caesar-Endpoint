49efb398ea3fbe86ae5e63d0be466f683faa35a7
Julian Weinelt <julian.weinelt@outlook.de>
2025-06-19 23:17:16 +0200

    chore(add getters and setters)

--- src/main/java/de/julianweinelt/caesar/web/PluginEntry.java
+++ src/main/java/de/julianweinelt/caesar/web/PluginEntry.java
@@ -1,7 +1,12 @@
 package de.julianweinelt.caesar.web;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
 public class PluginEntry {
     private String name;
     private String version;
