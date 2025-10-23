9c6472faafb9a7d8314505e4fe6f260896cb8261
Julian Weinelt <julian.weinelt@outlook.de>
2025-10-23 16:47:22 +0200

    improvement: cleaner data

--- src/main/resources/app/view/view_page.js
+++ src/main/resources/app/view/view_page.js
@@ -1,5 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 
+let fetchedData = {}
+
 document.addEventListener('DOMContentLoaded', function() {
 
     console.log(urlParams.get("plugin"));
@@ -14,47 +16,52 @@ document.addEventListener('DOMContentLoaded', function() {
             return response.json();
         })
         .then(data => {
-            console.log('Antwort erhalten:', data);
-            document.getElementById("plugin-version").textContent = "Version " + data.version;
-            document.getElementById('downloads').textContent = data.downloads + "";
-            document.getElementById("last-update").textContent = data.lastUpdated.substr(0, 12);
-            document.getElementById("creationDate").textContent = data.dateCreated.substr(0, 12);
+            fetchedData = data.plugin;
+
+            document.getElementById("plugin-version").textContent = "Version " + fetchedData.version;
+            document.getElementById('downloads').textContent = fetchedData.downloads + "";
+            document.getElementById("last-update").textContent = fetchedData.lastUpdated.substr(0, 12);
+            document.getElementById("creationDate").textContent = fetchedData.dateCreated.substr(0, 12);
             //if ("compatibleVersions" in data) document.getElementById("compatible-versions").textContent = "Caesar " + data.compatibleVersions[0];
-            document.getElementById("plugin-name").textContent = data.name;
-            document.getElementById("desc-short").textContent = data.description;
-            document.getElementById("desc-long").innerHTML = data.descriptionLong.replace('\n\r', '<br>');
-            document.getElementById("author").textContent = data.author;
-            if ("license" in data) {
-                document.getElementById("license").textContent = data.license;
+            document.getElementById("plugin-name").textContent = fetchedData.name;
+            document.getElementById("desc-short").textContent = fetchedData.description;
+            document.getElementById("desc-long").innerHTML = fetchedData.descriptionLong.replace('\n\r', '<br>');
+            document.getElementById("author").textContent = fetchedData.author;
+            document.getElementById("author").href = "/dev-profile?user=" + fetchedData.author;
+            if (fetchedData.author === getCookie("username")) {
+                document.getElementById('editBTN').classList.remove("hidden");
+            }
+            if ("license" in fetchedData) {
+                document.getElementById("license").textContent = fetchedData.license;
             } else {
                 document.getElementById("license-line").classList.add("hidden");
             }
 
-            if ("sourceCode" in data && data.sourceCode !== "") {
+            if ("sourceCode" in fetchedData && fetchedData.sourceCode !== "") {
                 document.getElementById("source-code-link").innerHTML = `<i class="fa-brands fa-github"></i> GitHub`;
-                document.getElementById("source-code-link").href = data.sourceCode;
+                document.getElementById("source-code-link").href = fetchedData.sourceCode;
             } else {
                 document.getElementById("source-code").classList.add("hidden");
             }
 
-            if ("sponsorLink" in data && data.sponsorLink !== "") {
-                document.getElementById("sponsor-link").href = data.sponsorLink;
-                document.getElementById("sponsor-link").textContent = data.sponsorLink;
+            if ("sponsorLink" in fetchedData && fetchedData.sponsorLink !== "") {
+                document.getElementById("sponsor-link").href = fetchedData.sponsorLink;
+                document.getElementById("sponsor-link").textContent = fetchedData.sponsorLink;
             } else {
                 document.getElementById("sponsor").classList.add("hidden");
             }
 
-            if ("wikiLink" in data && data.wikiLink !== "") {
-                document.getElementById("wiki-link").href = data.wikiLink;
-                document.getElementById("wiki-link").textContent = data.wikiLink;
+            if ("wikiLink" in fetchedData && fetchedData.wikiLink !== "") {
+                document.getElementById("wiki-link").href = fetchedData.wikiLink;
+                document.getElementById("wiki-link").textContent = fetchedData.wikiLink;
             } else {
                 document.getElementById("wiki").classList.add("hidden");
             }
-            document.getElementById("pl-logo").src = `http://${ADDRESS}/api/image/` + data.uniqueId + "?type=plogo";
-            if ("screenshots" in data) {
-                document.getElementById("screen1").src = `http://${ADDRESS}/api/image/` + data.screenshots[0] + "?type=screenshot";
-                if (data.screenshots.length >= 2) {
-                    document.getElementById("screen2").src = `http://${ADDRESS}/api/image/` + data.screenshots[1] + "?type=screenshot";
+            document.getElementById("pl-logo").src = `http://${ADDRESS}/api/image/` + fetchedData.uniqueId + "?type=plogo";
+            if ("screenshots" in fetchedData) {
+                document.getElementById("screen1").src = `http://${ADDRESS}/api/image/` + fetchedData.screenshots[0] + "?type=screenshot";
+                if (fetchedData.screenshots.length >= 2) {
+                    document.getElementById("screen2").src = `http://${ADDRESS}/api/image/` + fetchedData.screenshots[1] + "?type=screenshot";
                 } else document.getElementById("screen2").classList.add("hidden");
             }
         })
@@ -72,6 +79,10 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+function editPage() {
+    window.location.href = "/edit?plugin=" + fetchedData.uniqueId;
+}
 
 function startWebSocket() {
     const socket = new WebSocket("ws://localhost:41824");
